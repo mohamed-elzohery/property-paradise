e919a61dc1c4fc865daf3d43809a0dc7a88b69bf
Mohamed Elzohery <m.zohery1998@gmail.com>
2024-02-27 16:11:16 +0200

    feat(navbar): finsihing dummy auth logic

---
+++ src/ui/nav/NavLink.tsx
@@ -0,0 +1,38 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { Navlinks } from "./Navlinks";
+
+type NavLink = Omit<(typeof Navlinks)[0], "requiresLogin"> & {
+  isVisible: boolean;
+};
+
+interface NavLinkProps {
+  link: NavLink;
+  className?: string;
+  activeClassNames?: string;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({
+  link: { isVisible, name, path },
+  activeClassNames,
+  className,
+}) => {
+  const currentPathname = usePathname();
+  const linkPath = path;
+  const isActive = linkPath === currentPathname;
+
+  if (!isVisible) return null;
+
+  return (
+    <Link
+      href={linkPath}
+      className={`${isActive ? activeClassNames : ""} ${className}`}
+    >
+      {name}
+    </Link>
+  );
+};
+
+export default NavLink;
---
+++ src/ui/nav/Navlinks.ts
@@ -0,0 +1,17 @@
+export const Navlinks = [
+  {
+    name: "Home",
+    path: "/",
+    requiresLogin: false,
+  },
+  {
+    name: "Properties",
+    path: "/properties",
+    requiresLogin: false,
+  },
+  {
+    name: "Add Properties",
+    path: "/properties/add",
+    requiresLogin: true,
+  },
+];
