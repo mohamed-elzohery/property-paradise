aea403bc10a1f4d5b5bddc27620369aaf119c3b9
Mohamed Elzohery <m.zohery1998@gmail.com>
2024-03-02 15:54:50 +0200

    env: adding env types

---
+++ variables.d.ts
@@ -0,0 +1,13 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    NEXT_PUBLIC_API_DOMAIN: string;
+    CLOUDINARY_KEY: string;
+    CLOUDINARY_NAME: string;
+    CLOUDINARY_SECRET: string;
+    NEXTAUTH_SECRET: string;
+    CLOUDINARY__FOLDER_NAME: string;
+    MONGODB_URI: string;
+  }
+}
